c70bed30ee0c5ca5b4b7c866b9387b33a4eca2b1
Jingqi Fan <jingqi_fan@163.com>
2024-11-09 15:52:47 +0800

    Add files via upload

---
+++ App.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+class AppUser {
+    
+    var nameString: String?
+    var emailString: String?
+    var phoneString: String?
+    var blockString: String?
+    
+    var id: String?
+    
+}
+
+extension AppUser {
+    
+    static func transformUser(dict: [String: Any], key: String) -> AppUser {
+        
+        let user = AppUser()
+        user.nameString = dict["1) Name"] as? String
+        user.emailString = dict["2) Email"] as? String
+        user.phoneString = dict["3) Phone"] as? String
+        user.blockString = dict["4) Block"] as? String
+        
+        user.id = key
+        return user
+        
+    }
+    
+}   // #38
